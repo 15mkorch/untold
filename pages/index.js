e5fb19c9eed4f38ba95d99d833ccd8513282b0fb
Mary <15mkorch@gmail.com>
2020-11-17 22:25:45 -0500

    join our community button

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function Home() {
         <div>
           <h1> Untold </h1>
           <h2>Believing in the power of the untold story</h2>
+          <a href="https://mary230542.typeform.com/to/K55ckRqP" target="blank"><button >Join our Community</button></a>
         </div>
       </div>
       <img className="picture" src="/home_image.png" />
