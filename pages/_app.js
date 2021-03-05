3385c0ac6debc9fe61f0cf4704199fa4117d1657
Mary <15mkorch@gmail.com>
2021-03-05 15:36:28 -0400

    metadata edits

--- pages/_app.js
+++ pages/_app.js
@@ -18,10 +18,10 @@ class MyApp extends App {
         <Head>
           <title>the Untold Story Project</title>
           <link rel="icon" href="/favicon.ico" />
-          <meta property="og:title" content="The Untold Story Project - Raising Awareness of Abuse and Recovery" />
-          <meta property="og:description" content="The Untold Story Project - Raising Awareness of Abuse and Recovery" />
-          <meta property="og:url" content="https://www.untoldstoryproject.org/" />
-          <meta property="og:image" content="https://www.untoldstoryproject.org/" />
+          <meta property="og:title" content="The Untold Story Project - A podcast that finds strength in sharing the untold" />
+          <meta property="og:description" content="Some of the most powerful stories go untold. I hope to change that. We share stories on our podcast that are close to us. They have shaped who we have become, and can be a lot to swallow. There’s a hundred reasons as to why we wouldn’t talk about family, brokenness, and redemption, but that doesn’t mean we can’t find healing or even joy in stepping out. " />
+          <meta property="og:url" content="https://untoldstoryproject.org/" />
+          <meta property="og:image" content="https://untoldstoryproject.org/" />
 
         </Head>
         <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,15 @@ export default function Home() {
     <div id="index-background" className="background">
       <Head>
         <title>
-          The Untold Story Project-- Raising awareness of narcissism and abuse
+          The Untold Story Project-- A podcast that finds strength in sharing the untold
         </title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:image" content="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="The Untold Story Project believes in the power of stories that are kept under the rug. Stories of narcissism, domestic abuse, emotional abuse, and manipulation are difficult circumstances to reconcile and navigate. By telling our stories, we desire to bring empathy, education, and most of all hope to people who have shared in these experiences and those who haven’t. " />
-        <meta property="og:title" content="The Untold Story Project-- Raising awareness of narcissism and abuse" />
+        <meta name="description" content="Some of the most powerful stories go untold. I hope to change that. We share stories on our podcast that are close to us. They have shaped who we have become, and can be a lot to swallow. There’s a hundred reasons as to why we wouldn’t talk about family, brokenness, and redemption, but that doesn’t mean we can’t find healing or even joy in stepping out." />
+        <meta property="og:title" content="The Untold Story Project-- A podcast that finds strength in sharing the untold" />
 
       </Head>
       <Navbar />
